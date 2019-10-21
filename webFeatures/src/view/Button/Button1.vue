bc2cd689682875086b0370ca7b93cb75fa9aac7e
Qiang-Lee <hliqiang1989@gmail.com>
2019-10-21 16:18:30 +0800

    新增组件

---
+++ webFeatures/src/view/Button/Button1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <p>使用<code>type</code>、<code>plain</code>、<code>round</code>和<code>circle</code>属性来定义 Button 的样式。</p>
+    <el-collapse>
+      <el-collapse-item title="显示代码" name="1">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 25, maxRows: 4}"
+          readonly
+          placeholder="请输入内容"
+          v-model="textarea2"
+        ></el-input>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    }
+  },
+  data() {
+    return {
+      textarea2: `
+                <template>
+                   <div>
+                    <el-row>
+                        <el-button>默认按钮</el-button>
+                        <el-button type="primary">主要按钮</el-button>
+                        <el-button type="success">成功按钮</el-button>
+                        <el-button type="info">信息按钮</el-button>
+                        <el-button type="warning">警告按钮</el-button>
+                        <el-button type="danger">危险按钮</el-button>
+                    </el-row>
+                   </div>
+                </template>
+            `
+    };
+  }
+};
+</script>
+<style scoped>
+
+</style>
---
+++ webFeatures/src/view/Select/Select1.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+    </el-select>
+    <p>
+      <code>v-model</code>的值为当前被选中的
+      <code>el-option</code>的 value 属性值
+    </p>
+    <el-collapse>
+      <el-collapse-item title="显示代码" name="1">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 25, maxRows: 4}"
+          readonly
+          placeholder="请输入内容"
+          v-model="textarea2"
+        ></el-input>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    }
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕"
+        },
+        {
+          value: "选项2",
+          label: "双皮奶"
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎"
+        },
+        {
+          value: "选项4",
+          label: "龙须面"
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭"
+        }
+      ],
+      value: "",
+      textarea2: `
+                <template>
+                   <div>
+                      <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                        </el-option>
+                      </el-select>
+                   </div> 
+                </template>
+                <script>
+                  export default {
+                    data() {
+                      return {
+                        options: [{
+                          value: '选项1',
+                          label: '黄金糕'
+                        }, {
+                          value: '选项2',
+                          label: '双皮奶'
+                        }, {
+                          value: '选项3',
+                          label: '蚵仔煎'
+                        }, {
+                          value: '选项4',
+                          label: '龙须面'
+                        }, {
+                          value: '选项5',
+                          label: '北京烤鸭'
+                        }],
+                        value: ''
+                      }
+                    }
+                  }
+                <\/script>
+            `
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
