2527844e4324bb557be8938709766642aa723959
huangfangfang0217 <1078133629@qq.com>
2019-10-22 10:41:44 +0800

    添加日期

--- webFeatures/src/view/DatePicker/DatePicker2.vue
+++ webFeatures/src/view/DatePicker/DatePicker2.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <div class="block">
-      <span class="demonstration">起始日期时刻为 12:00:00，结束日期时刻为 08:00:00</span>
-      <el-date-picker
-        v-model="value2"
-        type="datetimerange"
-        align="right"
-        start-placeholder="开始日期"
-        end-placeholder="结束日期"
-        :default-time="['12:00:00', '08:00:00']"
-      ></el-date-picker>
-    </div>
-    <p>
-      使用
-      <code>datetimerange</code>进行范围选择时，在日期选择面板中选定起始与结束的日期，默认会使用该日期的
-      <code>00:00:00</code>作为起始与结束的时刻；通过选项
-      <code>default-time</code>可以控制选中起始与结束日期时所使用的具体时刻。
-      <code>default-time</code>接受一个数组，数组每项值为字符串，形如
-      <code>12:00:00</code>，其中第一项控制起始日期的具体时刻，第二项控制结束日期的具体时刻。
-    </p>
+     <el-time-picker placeholder="选择时间" v-model="value2"></el-time-picker>
     <el-collapse>
       <el-collapse-item title="显示代码" name="1">
         <el-input
@@ -40,19 +22,12 @@ export default {
       value2: "",
       textarea2: `
                 <template>
-                   <div>
-                     <div class="block">
-                        <span class="demonstration">起始日期时刻为 12:00:00，结束日期时刻为 08:00:00</span>
-                        <el-date-picker
-                          v-model="value2"
-                          type="datetimerange"
-                          align="right"
-                          start-placeholder="开始日期"
-                          end-placeholder="结束日期"
-                          :default-time="['12:00:00', '08:00:00']">
-                        </el-date-picker>
-                      </div>
-                   </div>
+                   <el-date-picker
+                      v-model="value2"
+                      type="date"
+                      align="right"
+                      placeholder="选择日期" 
+                    ></el-date-picker>
                 </template>
 
                 <script>
