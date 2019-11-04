40b4517b06ba3edc4bf17f7ef535ef5a6da42d73
woshi005441 <13310974593@163.com>
2019-11-04 15:42:30 +0800

    更改表单验证规则

--- hyrenweb/src/utils/js/regular.js
+++ hyrenweb/src/utils/js/regular.js
@@ -14,6 +14,7 @@ const regular = {
     mobilePhone: { regex: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/, error: '无效的手机号码', title: '手机' },
     accountNumber: { regex: /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/, error: '帐号不合法(以字母开头，允许5-16字符，允许字母数字下划线)', title: '账号' },
     password: { regex: /^[a-zA-Z]\w{5,17}$/, error: '密码不合法(以字母开头，长度在6~18字符之间，只能包含字母、数字和下划线)', title: '密码' },
+    dataScourenum: { regex: /^[a-zA-Z][a-zA-z0-9]{3}$/, error: '编号不合法(以字母开头的不超过四位数的字母数字组合)', title: '编号' },
 }
 
 
