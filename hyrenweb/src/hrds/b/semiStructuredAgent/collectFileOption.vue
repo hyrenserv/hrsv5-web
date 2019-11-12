613934775a12e857df9b854f3365af52886cb484
woshi005441 <13310974593@163.com>
2019-11-12 18:22:46 +0800

    增加新的页面

--- hyrenweb/src/hrds/b/semiStructuredAgent/collectFileOption.vue
+++ hyrenweb/src/hrds/b/semiStructuredAgent/collectFileOption.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>
-      111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-        111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-          111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-            111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-  </div>
+<div class="collectFileOption">
+    <el-row><i class="fa fa-signal"></i>采集文件列表</el-row>
+    <el-row>
+        <el-button size="medium" type="success" @click="addOneColumn">添加</el-button>
+    </el-row>
+    <el-table :data="tableData" border stripe size="mini">
+        <el-table-column type="index" label="序号" width="64" align="center"></el-table-column>
+        <el-table-column prop="datasource_name" label="英文名" align="center">
+            <template slot-scope="scope">
+                <el-input v-model="scope.row.tableChName" placeholder="英文名"></el-input>
+            </template></el-table-column>
+        <el-table-column prop="dep_name" label="中文名" align="center">
+            <template slot-scope="scope">
+                <el-input v-model="scope.row.tableChName" placeholder="中文名"></el-input>
+            </template></el-table-column>
+        <el-table-column label="数据类型" align="center">
+            <template slot-scope="scope">
+                <el-select v-model="form.region" placeholder="请选择活动区域" style="width: 100%;">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </template>
+        </el-table-column>
+        <el-table-column prop="datasource_name" label="数据更新方式" align="center">
+            <template slot-scope="scope">
+                <el-select v-model="form.region" placeholder="请选择活动区域" style="width: 100%;">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </template>
+        </el-table-column>
+        <el-table-column prop="dep_name" label="数据字符编码" align="center">
+            <template slot-scope="scope">
+                <el-select v-model="form.region" placeholder="请选择活动区域" style="width: 100%;">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </template>
+        </el-table-column>
+        <el-table-column label="操作" width="96" align="center">
+            <template slot-scope="scope">
+                <b @click="dialogFormVisibleAdd = true;handleEdit(scope.$index, scope.row);departmentInfo()">删除</b>
+            </template>
+        </el-table-column>
+    </el-table>
+</div>
 </template>
 
 <script>
@@ -13,6 +53,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.collectFileOption .el-row {
+    margin-top: 20px;
+}
 </style>
