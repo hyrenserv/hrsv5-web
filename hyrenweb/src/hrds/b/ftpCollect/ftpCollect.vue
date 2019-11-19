9f17aafb7939cfc577c939420e5d8ff4c69f9c9d
woshi005441 <13310974593@163.com>
2019-11-19 18:04:00 +0800

    添加树形结构

--- hyrenweb/src/hrds/b/ftpCollect/ftpCollect.vue
+++ hyrenweb/src/hrds/b/ftpCollect/ftpCollect.vue
@@ -1,8 +1,6 @@
 <template>
 <div class="ftpCollect">
-    <el-row>
-
-    </el-row>
+    <el-row></el-row>
 
     <el-row class="partTwo">
         <el-form ref="form" :model="form">
@@ -107,8 +105,7 @@
             <el-col :span="12">
                 <el-form-item label="下级目录规则" :label-width="formLabelWidth">
                     <el-select v-model="form.ftp_rule_path" placeholder="请选择数据下级目录规则" clearable style="width: 100%;">
-                        <el-option v-for="item in FtpRule" :key="item.value" :label="item.value" :value="item.code">
-                        </el-option>
+                        <el-option v-for="item in FtpRule" :key="item.value" :label="item.value" :value="item.code"></el-option>
                     </el-select>
                 </el-form-item>
             </el-col>
@@ -124,8 +121,7 @@
             <el-col :span="12" v-if="showOrHidden_reduce">
                 <el-form-item label="解压方式" :label-width="formLabelWidth">
                     <el-select v-model="form.reduce_type" placeholder="请选择解压方式" clearable style="width: 100%;">
-                        <el-option v-for="item in reduceType" :key="item.value" :label="item.value" :value="item.code">
-                        </el-option>
+                        <el-option v-for="item in reduceType" :key="item.value" :label="item.value" :value="item.code"></el-option>
                     </el-select>
                 </el-form-item>
             </el-col>
@@ -139,31 +135,39 @@
             <el-col :span="12">
                 <el-form-item label="启动方式" :label-width="formLabelWidth">
                     <el-select v-model="form.run_way" placeholder="请选择启动方式" clearable style="width: 100%;">
-                        <el-option v-for="item in runWay" :key="item.value" :label="item.value" :value="item.code">
-                        </el-option>
+                        <el-option v-for="item in runWay" :key="item.value" :label="item.value" :value="item.code"></el-option>
                     </el-select>
                 </el-form-item>
+            </el-col>
+
+            <el-col :span="12">
+                <el-tree :data="data2" show-checkbox :props="defaultProps" @node-click="handleNodeClick"></el-tree>
             </el-col>
         </el-form>
     </el-row>
 
     <el-row class="partThree">
         <el-col :span="12">
-            <el-button size="medium" type="primary" @click="goBackQuit"> 返回</el-button>
+            <el-button size="medium" type="primary" @click="goBackQuit">返回</el-button>
         </el-col>
 
         <el-col :span="12">
             <div class="partThreeDiv">
-                <el-button size="medium" type="success" style="float:right" @click="addFtpCollect">完成<i class="el-icon-check"></i></el-button>
+                <el-button size="medium" type="success" style="float:right" @click="addFtpCollect">
+                    完成
+                    <i class="el-icon-check"></i>
+                </el-button>
             </div>
         </el-col>
     </el-row>
-
 </div>
 </template>
 
 <script>
 import * as functionAll from "./ftpCollect";
+import {
+    log
+} from 'util';
 export default {
     data() {
         return {
@@ -177,34 +181,43 @@ export default {
             runWay: [],
             YesNo: [],
             reduceType: [],
+            data2: [
+
+            ],
+            item: {},
+            defaultProps: {
+                children: "children",
+                label: "path"
+            },
             disabled: true,
             showOrHidden_realtime: false,
             showOrHidden_reduce: false,
             showOrHidden_unzip: false,
             size: "medium",
             params: "",
-            formLabelWidth: "150px"
-        }
+            formLabelWidth: "150px",
+
+        };
     },
     created() {
-        // //// 是否实时读取判断是否实时读取间隔时间
+        //  是否实时读取判断是否实时读取间隔时间
         if (this.form.is_read_realtime == "0") {
             this.showOrHidden_realtime = false;
         } else if (this.form.is_read_realtime == "1") {
             this.showOrHidden_realtime = true;
-        };
+        }
         // 是否解压控制解压类型
         if (this.form.is_unzip == "0") {
             this.showOrHidden_reduce = false;
         } else if (this.form.is_unzip == "1") {
             this.showOrHidden_reduce = true;
-        };
+        }
         // 是否推拉模式控制是否解压
         if (this.form.ftp_model == "0") {
             this.showOrHidden_unzip = true;
         } else if (this.form.ftp_model == "1") {
             this.showOrHidden_unzip = false;
-        };
+        }
         this.getCategoryItems("IsFlag");
         this.getCategoryItems("ExecuteWay");
         this.getCategoryItems("FtpRule");
@@ -214,53 +227,57 @@ export default {
         goBackQuit() {
             this.$router.push({
                 name: "agentList"
-            })
+            });
         },
         // 获取代码项对应的值
         getCategoryItems(e) {
             if (e == "FtpRule") {
-                functionAll.getCategoryItems({
-                    category: e
-                }).then((res) => {
-                    if (res && res.success) {
-                        this.FtpRule = res.data;
-                    }
-                })
+                functionAll
+                    .getCategoryItems({
+                        category: e
+                    })
+                    .then(res => {
+                        if (res && res.success) {
+                            this.FtpRule = res.data;
+                        }
+                    });
             } else if (e == "ReduceType") {
-                functionAll.getCategoryItems({
-                    category: e
-                }).then((res) => {
-                    if (res && res.success) {
-                        this.reduceType = res.data;
-                    }
-                })
+                functionAll
+                    .getCategoryItems({
+                        category: e
+                    })
+                    .then(res => {
+                        if (res && res.success) {
+                            this.reduceType = res.data;
+                        }
+                    });
             } else if (e == "ExecuteWay") {
-                functionAll.getCategoryItems({
-                    category: e
-                }).then((res) => {
-                    if (res && res.success) {
-                        this.runWay = res.data;
-                    }
-                })
+                functionAll
+                    .getCategoryItems({
+                        category: e
+                    })
+                    .then(res => {
+                        if (res && res.success) {
+                            this.runWay = res.data;
+                        }
+                    });
             } else if (e == "IsFlag") {
-                functionAll.getCategoryItems({
-                    category: e
-                }).then((res) => {
-                    if (res && res.success) {
-                        this.YesNo = res.data;
-                    }
-                })
+                functionAll
+                    .getCategoryItems({
+                        category: e
+                    })
+                    .then(res => {
+                        if (res && res.success) {
+                            this.YesNo = res.data;
+                        }
+                    });
             }
         },
         // 添加ftp收集任务
         addFtpCollect() {
-            functionAll.addFtp_collect(
-                this.form
-            ).then((res) => {
-                if (res && res.success) {
-
-                }
-            })
+            functionAll.addFtp_collect(this.form).then(res => {
+                if (res && res.success) {}
+            });
         },
         // 是否实时读取控制实时读取间隔时间
         handerChange_realtime(val) {
@@ -288,23 +305,85 @@ export default {
             }
         },
         // 获取目录结构
-        seletFilePath() {
-            let arry = []
-            functionAll.selectPath({
-                agent_id: this.$route.query.agent_id,
-                path: "/"
-            }).then((res) => {
-                if (res && res.success) {
-                    for (let i = 0; i < res.data.length; i++) {
-                        arry.push({child:res.data[i].split("^")[0]});
+        seletFilePath(data) {
+            let arry = [];
+            let path; 
+            console.log(data)
+            if (typeof (data) != "undefined") {
+              path =  data.path;
+            }
+            functionAll
+                .selectPath({
+                    agent_id: this.$route.query.agent_id,
+                    path: "path"
+                })
+                .then(res => {
+                    if (typeof (data) == 'undefined') {
+                        // this.data2 = [{
+                        //     label: '/'命令行呢
+                        // }];
+                        this.data2 = res.data;
+                    } else if (typeof (data.path) != "undefined") {
+                        // let itme  = {}
+                            //第一层
+                        // this.data2.map(v => {
+                        //     //返回数据
+                            
+                        //     if (v.path == label) {
+                        //         itme['children']=res.data;
+                        //         this.data2.$set() = res.data;
+                        //         return;
+                        //     }
+                        
+                        // })
+                        data['children'] = res.data;
+                        console.log(data)
+                        // this.toolkit(this.data2,res.data)
+
                     }
-                    console.log(arry);
+                });
+        },
+        // 处理后台数据
+        toolkit(target, data) {
+            this.data2 = target;
+            console.log(this.data2)
+            data = data.map(function (p) {
+                return {
+                    label: p.path
                 }
             });
-        }
-    }
+            let flagStr = data[0].label.slice(0, data[0].label.lastIndexOf('/'));
 
-}
+            function filter(t) {
+                let index = t.findIndex(function (p, i) {
+                    return p.label.startsWith(flagStr);
+                });
+                if (index !== -1) {
+                    t[index].children = data;
+                } else if (t.children) {
+                    filter(target[children]);
+                } else {
+                    console.log(`target中没有label以flagStr开头的对象`);
+                }
+            }
+            filter(target);
+        },
+        // 获取目录下一级
+        handleNodeClick(data) {
+            this.seletFilePath(data);
+        }
+        // 拼接子目录
+        // append(data, parentNode) {
+        //     let arry2 = [{label:parentNode,children:[]}]
+        //     for (let i = 0; i < data.length; i++) {
+        //         arry2[0].children.push({
+        //             label: data[i].label
+        //         })
+        //     }
+        //     this.data = arry2;
+        // }
+    }
+};
 </script>
 
 <style scoped>
