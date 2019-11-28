c621f5b32f268930987ac039eae79cd7b0578e7a
xchao <xchao_lr@126.com>
2019-11-28 14:33:43 +0800

    修改路由样式

---
+++ hyrenweb/src/hrds/components/notFound.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="error">
+        <br><br>
+        <br><br>
+        <br><br>
+        <span class="aaa">404</span> <br><br><br><br><br><br>
+        <span class="bbb">系統正在建设中......</span>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .error{
+        text-align: center;
+        margin-top: 25px;
+        height: 600px;
+        .aaa{
+            
+            font-size: 250px;
+            color: white;
+            font-family: '方正舒体'
+        }
+        .bbb{
+ 
+            font-size: 80px;
+            color: white;
+            font-family: '方正舒体'
+        }
+    }
+</style>
--- hyrenweb/src/router/index.js
+++ hyrenweb/src/router/index.js
@@ -14,177 +14,38 @@ Vue.use(Router)
  *  }
  * 
  */
-export default new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '*',
-            name: 'login',
-            component: () => import('@/hrds/login/login.vue')
-        },
-        {
-            path: '/',
-            name: 'login',
-            component: () => import('@/hrds/login/login.vue')
-        },
-        //菜单路由地址配置
-        {
-            path: "/home",
-            name: 'home',
-            component: () => import('@/hrds/components/menu'),
-            children: [
-                {
-                    path: '/syspara',
-                    name: 'syspara',
-                    component: () => import('@/hrds/a/syspara/index.vue')
-                },
-                {
-                    path: '/agentdeploy',
-                    name: 'agentdeploy',
-                    component: () => import('@/hrds/b/agentdeploy/agentdeploylist.vue')
-                },
-                {
-                    path: '/collectmonitor',
-                    name: 'collectmonitor',
-                    title: '采集监控首页',
-                    component: () => import('@/hrds/b/collectmonitor/collectmonitor.vue')
-                },
-                {
-                    path: '/datasourceManagement',
-                    name: ' datasourceManagement',
-                    title: '数据源管理',
-                    component: () => import('@/hrds/b/datasource/datasourceManagement.vue')
-                },
-                {
-                    path: '/addScoure',
-                    name: 'addScoure',
-                    title: 'agent管理',
-                    component: () => import('@/hrds/b/datasource/datasource.vue')
-                },
-                {
-                    path: '/dbaddTasksteps01',
-                    name: 'addTask01',
-                    title: '数据库采集',
-                    component: () => import('@/hrds/b/addTask/steps1.vue')
-                },
-                {
-                    path: '/dbaddTasksteps02',
-                    name: 'addTask02',
-                    title: '数据库采集02',
-                    component: () => import('@/hrds/b/addTask/steps2.vue')
-                },
-                {
-                    path: '/dbaddTasksteps03',
-                    name: 'addTask03',
-                    title: '数据库采集03',
-                    component: () => import('@/hrds/b/addTask/steps3.vue')
-                },
-                {
-                    path: '/dbaddTasksteps04',
-                    name: 'addTask04',
-                    title: '数据库采集04',
-                    component: () => import('@/hrds/b/addTask/steps4.vue')
-                },
-                {
-                    path: '/dbaddTasksteps05',
-                    name: 'addTask05',
-                    title: '数据库采集05',
-                    component: () => import('@/hrds/b/addTask/steps5.vue')
-                },
-                {
-                    path: '/dbaddTasksteps06',
-                    name: 'addTask06',
-                    title: '数据库采集06',
-                    component: () => import('@/hrds/b/addTask/steps6.vue')
-                },
-                {
-                    path: '/agentList',
-                    name: 'agentList',
-                    title: 'agent列表',
-                    component: () => import('@/hrds/b/agentList/agentList.vue')
-                },
-                {
-
-                    path: '/taskLog',
-                    name: 'taskLog',
-                    title: 'taskLog日志查看',
-                    component: () => import('@/hrds/b/agentList/taskLog.vue')
-                 },
-                {
-                    path: '/managementsystem',
-                    name: 'managementsystem',
-                    title: '系统管理',
-                    component: () => import('@/hrds/a/managementsystem/managementsystem.vue')
-                },
-                {
-                    path: '/userManagement',
-                    name: 'userManagement',
-                    title: '用户列表',
-                    component: () => import('@/hrds/a/managementsystem/userManagement.vue')
-                },
-                {
-                    path: '/departmentalList',
-                    name: 'departmentalList',
-                    title: '部门列表',
-                    component: () => import('@/hrds/a/managementsystem/departmentalList.vue')
-
-                },
-                {
-                    path: '/systemParameters',
-                    name: 'systemParameters',
-                    title: '系统列表',
-                    component: () => import('@/hrds/a/managementsystem/systemParameters.vue')
-
-                },
-                // 半结构化agent采集
-                {
-                    path: '/collectOption',
-                    name: 'collectOption',
-                    title: '采集设置',
-                    component: () => import('@/hrds/b/semiStructuredAgent/collectOption.vue')
-                },
-                {
-                    path: '/collectFileOption',
-                    name: 'collectFileOption',
-                    title: '采集文件设置',
-                    component: () => import('@/hrds/b/semiStructuredAgent/collectFileOption.vue')
-                },
-                {
-                    path: '/collectionStructureSet',
-                    name: 'collectionStructureSet',
-                    title: '采集文件设置',
-                    component: () => import('@/hrds/b/semiStructuredAgent/collectionStructureSet.vue')
-                },
-
-                //ftp agent采集
-                {
-                    path: '/ftpCollect',
-                    name: 'ftpCollect',
-                    title: '采集ftp agent',
-                    component: () => import('@/hrds/b/ftpCollect/ftpCollect.vue')
-                },
-                // 非结构化 Agent采集
-                {
-                    path: '/configureStartMode',
-                    name: 'configureStartMode',
-                    title: '配置启动方式',
-                    component: () => import('@/hrds/b/unstructuredAgent/configureStartMode.vue')
-                },
-                {
-                    path: '/configureFileOption',
-                    name: 'configureFileOption',
-                    title: '配置连接属性',
-                    component: () => import('@/hrds/b/unstructuredAgent/configureFileOption.vue')
-                },
-                // dataStoreAction
-                {
-                    path: '/dataStoreAction',
-                    name: 'dataStoreAction',
-                    title: 'dataStoreAction',
-                    component: () => import('@/hrds/a/datastore/dataStoreAction.vue')
-                },
-            ]
-        },
-
-    ]
-})
+export default new Router({/*mode: 'history',*/routes: [
+    {path: '/',name: 'login',component: () => import('@/hrds/login/login.vue')},
+    //菜单路由地址配置
+    {path: "/home",name: 'home',component: () => import('@/hrds/components/menu'),children: [
+        {path: '/syspara',name: 'syspara',component: () => import('@/hrds/a/syspara/index.vue')},
+        {path: '/agentdeploy',name: 'agentdeploy',component: () => import('@/hrds/b/agentdeploy/agentdeploylist.vue')},
+        {path: '/collectmonitor',name: 'collectmonitor',title: '采集监控首页',component: () => import('@/hrds/b/collectmonitor/collectmonitor.vue')},
+        {path: '/datasourceManagement',name: ' datasourceManagement',title: '数据源管理',component: () => import('@/hrds/b/datasource/datasourceManagement.vue') },
+        {path: '/addScoure',name: 'addScoure',title: 'agent管理',component: () => import('@/hrds/b/datasource/datasource.vue')},
+        {path: '/dbaddTasksteps01',name: 'addTask01',title: '数据库采集',component: () => import('@/hrds/b/addTask/steps1.vue')},
+        {path: '/dbaddTasksteps02',name: 'addTask02',title: '数据库采集02',component: () => import('@/hrds/b/addTask/steps2.vue')},
+        {path: '/dbaddTasksteps03',name: 'addTask03',title: '数据库采集03',component: () => import('@/hrds/b/addTask/steps3.vue')},
+        {path: '/dbaddTasksteps04',name: 'addTask04',title: '数据库采集04',component: () => import('@/hrds/b/addTask/steps4.vue')},
+        {path: '/dbaddTasksteps05',name: 'addTask05',title: '数据库采集05',component: () => import('@/hrds/b/addTask/steps5.vue')},
+        {path: '/dbaddTasksteps06',name: 'addTask06',title: '数据库采集06',component: () => import('@/hrds/b/addTask/steps6.vue')},
+        {path: '/agentList',name: 'agentList',title: 'agent列表',component: () => import('@/hrds/b/agentList/agentList.vue')},
+        {path: '/taskLog',name: 'taskLog',title: 'taskLog日志查看',component: () => import('@/hrds/b/agentList/taskLog.vue')},
+        {path: '/managementsystem',name: 'managementsystem',title: '系统管理',component: () => import('@/hrds/a/managementsystem/managementsystem.vue')},
+        {path: '/userManagement',name: 'userManagement',title: '用户列表',component: () => import('@/hrds/a/managementsystem/userManagement.vue')},
+        {path: '/departmentalList',name: 'departmentalList',title: '部门列表',component: () => import('@/hrds/a/managementsystem/departmentalList.vue')},    
+        {path: '/systemParameters',name: 'systemParameters',title: '系统列表',component: () => import('@/hrds/a/managementsystem/systemParameters.vue')},
+        // 半结构化agent采集    
+        {path: '/collectOption',name: 'collectOption',title: '采集设置',component: () => import('@/hrds/b/semiStructuredAgent/collectOption.vue')},
+        {path: '/collectFileOption',name: 'collectFileOption',title: '采集文件设置',component: () => import('@/hrds/b/semiStructuredAgent/collectFileOption.vue')},
+        {path: '/collectionStructureSet',name: 'collectionStructureSet',title: '采集文件设置',component: () => import('@/hrds/b/semiStructuredAgent/collectionStructureSet.vue')},
+        //ftp agent采集
+        {path: '/ftpCollect',name: 'ftpCollect',title: '采集ftp agent',component: () => import('@/hrds/b/ftpCollect/ftpCollect.vue')},    
+        // 非结构化 Agent采集    
+        {path: '/configureStartMode',name: 'configureStartMode',title: '配置启动方式',component: () => import('@/hrds/b/unstructuredAgent/configureStartMode.vue')},
+        {path: '/configureFileOption',name: 'configureFileOption',title: '配置连接属性',component: () => import('@/hrds/b/unstructuredAgent/configureFileOption.vue')},
+        // dataStoreAction
+        {path: '/dataStoreAction',name: 'dataStoreAction',title: 'dataStoreAction',component: () => import('@/hrds/a/datastore/dataStoreAction.vue')},
+    ]}, 
+    {path: '*',name: '*',component: () => import('@/hrds/components/notFound.vue')},
+]})
