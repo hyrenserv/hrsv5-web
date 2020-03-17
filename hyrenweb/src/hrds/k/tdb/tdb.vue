1756c2fcc22e8cdc8639967d9f98ecf8d05cc85a
huangfangfang0217 <1078133629@qq.com>
2020-03-17 10:50:44 +0800

    k-表结构修改0317

--- hyrenweb/src/hrds/k/tdb/tdb.vue
+++ hyrenweb/src/hrds/k/tdb/tdb.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <el-row><h4 class='el-icon-s-home' style='color: #2196f3;
-    line-height: 30px;
-    padding-bottom: 10px;font-size: 18px;'>表数据对标</h4></el-row>
+        <el-row><h4 class='el-icon-s-home' >表数据对标</h4></el-row>
     </div>
 </template>
 <script>
 </script>
-<style scoped>
+<style scoped lang='less'>
+// @import '../../../assets/css/changeColor.less';
+
 </style>
