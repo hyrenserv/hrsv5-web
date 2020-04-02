8b47e49afda872d734c88dda1beded44097ec1c1
woshi005441 <13310974593@163.com>
2020-04-02 14:21:14 +0800

    修改分页数

--- hyrenweb/src/hrds/c/detailsPages/Configure/etlJobDef/etlJobDef.vue
+++ hyrenweb/src/hrds/c/detailsPages/Configure/etlJobDef/etlJobDef.vue
@@ -562,7 +562,7 @@ export default {
             },
             pagesize: 5,
             currpage: 1,
-            pageLength: 100,
+            pageLength: 0,
         };
     },
     mounted() {
