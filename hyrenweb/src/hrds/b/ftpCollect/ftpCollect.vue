c6572c8fe0e63f2c5fce4bc9acc2fa936f453bea
woshi005441 <13310974593@163.com>
2020-04-10 11:14:31 +0800

    xiugai

--- hyrenweb/src/hrds/b/ftpCollect/ftpCollect.vue
+++ hyrenweb/src/hrds/b/ftpCollect/ftpCollect.vue
@@ -148,12 +148,12 @@
 
     <el-row class="partThree">
         <el-col :span="12">
-            <el-button size="mini" type="primary" @click="goBackQuit">返回</el-button>
+            <el-button size="medium" type="primary" @click="goBackQuit">返回</el-button>
         </el-col>
 
         <el-col :span="12">
             <div class="partThreeDiv">
-                <el-button size="mini" type="success" style="float:right" @click="submitForm('form')">
+                <el-button size="medium" type="success" style="float:right" @click="submitForm('form')">
                     完成
                     <i class="el-icon-check"></i>
                 </el-button>
