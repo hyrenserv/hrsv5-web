324adcda48f7650c7a636b7412f5ad88ac2f0a8d
SHU_TBH <410297778@qq.com>
2020-04-17 17:19:46 +0800

    提交集市内容

--- hyrenweb/src/router/index.js
+++ hyrenweb/src/router/index.js
@@ -451,6 +451,12 @@ export default new Router({
                     component: () => import('@/hrds/h/market/addMartTable_2.vue')
                 },
                 {
+                    path: '/addMartTable_3',
+                    name: 'addMartTable_3',
+                    title: 'addMartTable_3',
+                    component: () => import('@/hrds/h/market/addMartTable_3.vue')
+                },
+                {
                     path: '/serviceMage',
                     name: 'serviceMage',
                     title: '服务接口管理',
