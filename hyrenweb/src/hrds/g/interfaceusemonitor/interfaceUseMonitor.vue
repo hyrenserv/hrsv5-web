d6696ad5dbacb4e84d88f6d1ead5e78b0581f35f
dhw <duhuawei@beyondsoft.com>
2020-05-19 14:46:12 +0800

    修改接口样式以及数据范围管理表对应列一对多

--- hyrenweb/src/hrds/g/interfaceusemonitor/interfaceUseMonitor.vue
+++ hyrenweb/src/hrds/g/interfaceusemonitor/interfaceUseMonitor.vue
@@ -40,7 +40,7 @@
                 <!--接口信息列表展示-->
                 <el-table :data="interfaceData.slice((currPage - 1) * pageSize,currPage * pageSize)"
                           border style="width: 100%" size="medium">
-                    <el-table-column label="序号" align="center">
+                    <el-table-column label="序号" width="50px" align="center">
                         <template slot-scope="scope">
                             <span>{{scope.$index+(currPage - 1) * pageSize + 1}}</span>
                         </template>
@@ -48,9 +48,11 @@
                     <el-table-column prop="interface_name" label="接口名称" align="center"/>
                     <el-table-column prop="interface_code" label="接口代码" align="center"/>
                     <el-table-column prop="user_name" label="使用用户" align="center"/>
-                    <el-table-column prop="start_use_date" label="开始日期" align="center"/>
-                    <el-table-column prop="use_valid_date" label="结束日期" align="center"/>
-                    <el-table-column prop="use_state" label="接口使用状态" align="center">
+                    <el-table-column prop="start_use_date" :formatter="dateFormat" label="开始日期"
+                                     align="center"/>
+                    <el-table-column prop="use_valid_date" :formatter="dateFormat" label="结束日期"
+                                     align="center"/>
+                    <el-table-column prop="use_state" width="80px" label="接口使用状态" align="center">
                         <template slot-scope="scope">{{useState[scope.row.use_state]}}</template>
                     </el-table-column>
                     <el-table-column label="操作" align="center">
@@ -104,7 +106,7 @@
                 </el-row>
                 <el-table :data="dataTableData.slice((currPage - 1) * pageSize,currPage * pageSize)"
                           border style="width: 100%" size="medium">
-                    <el-table-column label="序号" align="center">
+                    <el-table-column label="序号" width="50px" align="center">
                         <template slot-scope="scope">
                             <span>{{scope.$index+(currPage - 1) * pageSize + 1}}</span>
                         </template>
@@ -357,6 +359,7 @@
             },
             // 改变tabs框
             handleClick(tab) {
+                this.currPage = 1;
                 if (tab.paneName === "0") {
                     this.searchInterfaceInfo();
                 } else {
@@ -379,7 +382,17 @@
             resetDataTable() {
                 this.searchTableData();
                 this.user_id = '';
-            }
+            },
+            // 表格日期格式化展示
+            dateFormat(row, column) {
+                const date = row[column.property];
+                if (date != null) {
+                    const year = date.substring(0, 4);
+                    const month = date.substring(4, 6);
+                    const day = date.substring(6, 8);
+                    return year + "-" + month + "-" + day;
+                }
+            },
         }
     };
 </script>
