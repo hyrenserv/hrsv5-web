584d9ab2a0e5bced2068a3e1c0e91523e38054cb
dhw <duhuawei@beyondsoft.com>
2020-08-05 17:47:31 +0800

    修改作业调度页面展示面包屑

--- hyrenweb/src/router/index.js
+++ hyrenweb/src/router/index.js
@@ -487,7 +487,9 @@ export default new Router({
                     component: () => import('@/hrds/c/etlMage/etlMage.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '作业调度' },
+                        { name: '作业调度' ,url:'etlMage'},
+                        { name: '工程管理' },
+
                     ],
                 },
                 {
@@ -684,180 +686,195 @@ export default new Router({
                         {
                             path: '/currentBatch',
                             name: 'currentBatch',
-                            title: '工程详情',
+                            title: '当前批量',
                             component: () => import('@/hrds/c/detailsPages/Monitor/currentBatch/currentBatch.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '当前批量' }
                             ],
                         },
                         //监控历史批量详情页面
                         {
                             path: '/historyBatch',
                             name: 'historyBatch',
-                            title: '工程详情',
+                            title: '历史批量',
                             component: () => import('@/hrds/c/detailsPages/Monitor/historyBatch/historyBatch.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '历史批量' }
                             ],
                         },
                         //监控当前作业详情页面
                         {
                             path: '/currentJob',
                             name: 'currentJob',
-                            title: '工程详情',
+                            title: '当前作业',
                             component: () => import('@/hrds/c/detailsPages/Monitor/currentJob/currentJob.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '当前作业' }
                             ],
                         },
                         //监控历史作业详情页面
                         {
                             path: '/historyJob',
                             name: 'historyJob',
-                            title: '工程详情',
+                            title: '历史作业',
                             component: () => import('@/hrds/c/detailsPages/Monitor/historyJob/historyJob.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '历史作业' }
                             ],
                         },
                         //监控依赖作业详情页面
                         {
                             path: '/relyJob',
                             name: 'relyJob',
-                            title: '工程详情',
+                            title: '依赖作业',
                             component: () => import('@/hrds/c/detailsPages/Monitor/relyJob/relyJob.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '依赖作业' }
                             ],
                         },
                         //监控系统资源详情页面
                         {
                             path: '/systemResource',
                             name: 'systemResource',
-                            title: '工程详情',
+                            title: '系统资源',
                             component: () => import('@/hrds/c/detailsPages/Monitor/systemResource/systemResource.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '监控',url:'/menus' },
+                                { name: '系统资源' }
                             ],
                         },
                         //干预系统级干预详情页面
                         {
                             path: '/sysLevelIntervente',
                             name: 'sysLevelIntervente',
-                            title: '工程详情',
+                            title: '系统级干预',
                             component: () => import('@/hrds/c/detailsPages/Intervene/sysLevelIntervente/sysLevelIntervente.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '干预',url:'/menus' },
+                                { name: '系统级干预' }
                             ],
                         },
                         //干预作业级干预详情页面
                         {
                             path: '/jobLevelIntervente',
                             name: 'jobLevelIntervente',
-                            title: '工程详情',
+                            title: '作业级干预',
                             component: () => import('@/hrds/c/detailsPages/Intervene/jobLevelIntervente/jobLevelIntervente.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '干预',url:'/menus' },
+                                { name: '作业级干预' }
                             ],
                         },
                         //配置任务详情页面
                         {
                             path: '/subSystem',
                             name: 'subSystem',
-                            title: '工程详情',
+                            title: '任务',
                             component: () => import('@/hrds/c/detailsPages/Configure/subSystem/subSystem.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '任务' }
                             ],
                         },
                         //配置作业模板详情页面
                         {
                             path: '/etlJobDefTemplate',
                             name: 'etlJobDefTemplate',
-                            title: '工程详情',
+                            title: '作业模板',
                             component: () => import('@/hrds/c/detailsPages/Configure/etlJobDefTemplate/etlJobDefTemplate.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '作业模板' }
                             ],
                         },
                         //配置作业详情页面
                         {
                             path: '/etlJobDef',
                             name: 'etlJobDef',
-                            title: '工程详情',
+                            title: '作业',
                             component: () => import('@/hrds/c/detailsPages/Configure/etlJobDef/etlJobDef.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '作业' }
                             ],
                         },
                         //配置资源定义详情页面
                         {
                             path: '/resourcesAvailable',
                             name: 'resourcesAvailable',
-                            title: '工程详情',
+                            title: '资源定义',
                             component: () => import('@/hrds/c/detailsPages/Configure/resourcesAvailable/resourcesAvailable.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '资源定义' }
                             ],
                         },
                         //配置资源分配详情页面
                         {
                             path: '/resourcesUsage',
                             name: 'resourcesUsage',
-                            title: '工程详情',
+                            title: '资源分配',
                             component: () => import('@/hrds/c/detailsPages/Configure/resourcesUsage/resourcesUsage.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '资源分配' }
                             ],
                         },
                         //配置作业依赖详情页面
                         {
                             path: '/etlDependency',
                             name: 'etlDependency',
-                            title: '工程详情',
+                            title: '作业依赖',
                             component: () => import('@/hrds/c/detailsPages/Configure/etlDependency/etlDependency.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '作业依赖' }
                             ],
                         },
                         //配置系统参数详情页面
                         {
                             path: '/systemParameter',
                             name: 'systemParameter',
-                            title: '工程详情',
+                            title: '系统参数',
                             component: () => import('@/hrds/c/detailsPages/Configure/systemParameter/systemParameter.vue'),
                             meta: [
                                 { name: '首页' },
                                 { name: '作业调度', url: '/etlMage' },
-                                { name: '工程详情' },
+                                { name: '配置', url: '/menus' },
+                                { name: '系统参数' }
                             ],
                         },
                     ]
@@ -869,7 +886,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/marketIndex.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' },
+                        { name: '数据集市' },
                     ],
                 },
                 {
@@ -879,7 +896,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/addMarketIndex.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' },
+                        { name: '数据集市' },
                     ],
                 },
                 {
@@ -889,7 +906,7 @@ export default new Router({
                     component: () => import('@/hrds/h/manage/version/marketVersionManage.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' },
+                        { name: '数据集市' },
                         { name: '版本管理' },
                     ],
                 },
@@ -900,7 +917,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/detailMart.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' }
+                        { name: '数据集市' }
                     ],
                 },
                 {
@@ -910,7 +927,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/addMartTable_1.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' }
+                        { name: '数据集市' }
                     ],
                 },
                 {
@@ -920,7 +937,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/addMartTable_2.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' }
+                        { name: '数据集市' }
                     ],
                 },
                 {
@@ -930,7 +947,7 @@ export default new Router({
                     component: () => import('@/hrds/h/market/addMartTable_3.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' }
+                        { name: '数据集市' }
                     ],
                 },
                 {
@@ -940,7 +957,8 @@ export default new Router({
                     component: () => import('@/hrds/h/market/importReview.vue'),
                     meta: [
                         { name: '首页' },
-                        { name: '数据加工' },
+                        { name: '数据集市',url:'dataMart' },
+                        { name: '集市工程导入审核' },
                     ],
                 },
                 {
