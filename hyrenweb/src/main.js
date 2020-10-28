23ca6eccf35ec9b713129bc71c996a0f68038660
Qiang-Lee <hliqiang1989@gmail.com>
2020-10-28 11:04:33 +0800

    增加弹出层拖拽功能, 只需要在el-dialog标签中添加v-dialogDrag属性即可

--- hyrenweb/src/main.js
+++ hyrenweb/src/main.js
@@ -10,7 +10,7 @@ import App from './App.vue';
 import '@/assets/font-awesome-4.7.0/css/font-awesome.min.css'
 import '@/assets/icon/iconfont.css'
 Vue.use(ElementUI);
-ElementUI.Dialog.props.closeOnClickModal.default = false; //默认点击遮照为不关闭
+ElementUI.Dialog.props.closeOnClickModal.default = false; //默认点击遮照为不关闭
 /**验证引入 */
 import filterRule from '@/utils/js/filter_Rules'
 Vue.use(filterRule)
@@ -32,7 +32,7 @@ import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
 Vue.component('vue-draggable-resizable', VueDraggableResizable)
 // Base64
-import Base64 from '@/utils/base64.js' 
+import Base64 from '@/utils/base64.js'
 Vue.prototype.$Base64 = Base64;
 // 引入滚轮
 import { HappyScroll } from 'vue-happy-scroll'
@@ -48,6 +48,10 @@ import $ from 'jquery';
 /** 获取代码项类型 */
 import * as codeitems from '@/utils/js/codeitems.js'
 Vue.prototype.$Code = codeitems;
+/**
+ * 弹窗可拖动
+ */
+import '@/utils/js/directives.js'
 Vue.config.productionTip = false
 new Vue({
     el: '#app',
