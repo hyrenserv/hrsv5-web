9eb2ad287af95a027ba4426585095474ff706299
HLL <78733419@qq.com>
2021-02-23 16:38:24 +0800

    主键分析显示优化

--- hyrenweb/src/hrds/k/tdb_result/pk_analysis_result.vue
+++ hyrenweb/src/hrds/k/tdb_result/pk_analysis_result.vue
@@ -14,7 +14,7 @@
             <el-form :inline="true" :model="searchForm" ref="searchForm" class="demo-form-inline" size="mini">
                 <el-form-item label="表名" prop="table_code" title="表名称搜索">
                     <!-- <el-input type="text" placeholder="表名" v-model="searchForm.table_code" /> -->
-                    <el-select v-model="searchForm.table_code" filterable placeholder="请选择" clearable>
+                    <el-select v-model="searchForm.table_code" filterable placeholder="请选择">
                         <el-option v-for="item in table_code_s" :key="item.value" :label="item.label" :value="item.value"></el-option>
                     </el-select>
                 </el-form-item>
@@ -24,9 +24,10 @@
             </el-form>
         </el-col>
     </el-row>
+    <el-row class='topTitle'>表名: {{searchForm.table_code}}</el-row>
     <el-table :data="tableData" style="width: 100%">
         <el-table-column prop="col_num" align="center" label="序号" min-width="10%" sortable></el-table-column>
-        <el-table-column prop="table_code" align="center" label="表名" min-width="30%" sortable></el-table-column>
+        <!-- <el-table-column prop="table_code" align="center" label="表名" min-width="30%" sortable></el-table-column> -->
         <el-table-column prop="col_code" align="center" label="字段名" min-width="20%" sortable></el-table-column>
         <el-table-column prop="col_type" align="center" label="预测字段类型" min-width="20%" sortable></el-table-column>
         <el-table-column prop="col_nullable" align="center" label="是否可为空" min-width="10%" sortable></el-table-column>
