c6762d66f0cb6956bae039cf57409d1ae5053196
HLL <hulili@beyondsoft.com>
2021-03-30 16:52:55 +0800

    添加 J 项目菜单初始化页面

---
+++ hyrenweb/src/hrds/j/streamDataManagement/streamDataManagement.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <el-row class='topTitle'>
+            <span>流数据管理</span>
+            <router-link to="/streamDataManagement">
+                <el-button type="primary" size="small" class="goIndex">
+                    <i class="fa fa-home fa-lg"/>返回首页
+                </el-button>
+            </router-link>
+        </el-row>
+    </div>
+</template>
