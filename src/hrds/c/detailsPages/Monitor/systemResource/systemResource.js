9209d7ecc519bb8b91f948a2387d9a61adb0024c
HLL <hulili@beyondsoft.com>
2021-03-30 18:01:03 +0800

    回退

--- src/hrds/c/detailsPages/Monitor/systemResource/systemResource.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-// 监控当前批量
-export function monitorSystemResourceInfo(data) {
-    return request({
-        url: '/C/etlmonitor/monitorSystemResourceInfo',
-        method: 'post',
-        params: data
-    })
-}
